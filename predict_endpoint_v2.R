c8ec2365ecadbae94d8dd9a085326ef76999ae77
snheffer <snheffer@outlook.com>
2023-08-17 20:04:07 +0100

    Update Prediction API:
    - Move /train method, include R6 Class Import functionality to enable interchangeability of machine learning algorithms/models.
    
    TODO: obfuscate/abstract away the inner workings of the machine learning pipeline, and turn "pre_proc" and "main_proc" into methods, and call them from a public method that does everything for the user, given a list of (meta)data.

--- predict_endpoint_v2.R
+++ predict_endpoint_v2.R
@@ -1,4 +1,9 @@
 library(plumber)
+library(R6)
+library(parallel)
+library(future)
+# library(pls)
+# library(caret)
 utilities <- new.env()
 sys.source("./utilities/utilities_v2.R", envir = utilities, toplevel.env = utilities)
 
@@ -12,15 +17,17 @@ download_and_load_model <- function(url) {
 
 
 # Create a mock model with metadata
-mock_model <- list(
-  metadata = list(
-    dimensions = c("feature1", "feature2", "feature3")
-  ),
-  trained_model = "YourTrainedModelObject"
-)
+# mock_model <- list(
+#   metadata = list(
+#     dimensions = c("feature1", "feature2", "feature3")
+#   ),
+#   trained_model = "YourTrainedModelObject"
+# )
 
+Saved_Model<-readRDS("/home/user/Documents/projects/r.reproduce.foodspoilage.pls/chicken_reapplication_prediction_API_R6.RDS")
+saved_model<- Saved_Model$new("/home/user/Documents/projects/r.reproduce.foodspoilage.pls/chicken_beef_decision_fusion/chicken_reapplication_models.RDS")
 # Save the mock model as an RDS file
-saveRDS(mock_model, "mock_model.rds")
+# saveRDS(mock_model, "mock_model.rds")
 
 library(plumber)
 
@@ -35,7 +42,7 @@ loaded_model <- readRDS("mock_model.rds")
 #* @param file_list:[file]
 #* @param predictor:name
 
-#* @post /predict
+#* @post /train
 function(file_list, predictor=NULL) {
   # read_file<-  read.table(text = file_list[[1]], sep =",", header = TRUE, stringsAsFactors = FALSE)
   # loaded_model <- readRDS(url)
@@ -52,53 +59,62 @@ function(file_list, predictor=NULL) {
   return(list(prediction = prediction, read_files = read_files, body = body))
 }
 
-#* @post /train
+#* @post /predict
 #* @param file_list:[file]
 #* @param predictor:name
-#* @serializer csv
+#* @serializer json
 function(file_list, predictor=NULL) {
-  read_files<- list()
-  for (i in 1:length(file_list)){
-    # cat(file_list[[i]])
-    read_files[[names(file_list)[i]]]<- read.table(text = file_list[[i]], sep =",", header = TRUE, stringsAsFactors = FALSE)
-    colnames(read_files[[names(file_list)[i]]])[1]<-"Sample"
-    cat("COLNAMES:\n")
-    cat(colnames(read_files[[names(file_list)[i]]]))
-    cat("\n")
-  }
-  model_metadata<- list()
-  model_metadata$xlabels<-list()
-  predictor_vector<- NULL
-  read_files.x<- list()
-  read_files.y<- list()
-  if (!is.null(predictor) && length(read_files)){
-    predictor_vector<- read_files[[names(file_list)[1]]][,c("Sample",predictor)]
-  }
-  if(length(read_files)){
-    for(i in 1:length(read_files)){
-      read_files.x[[i]]<-read_files[[names(read_files)[i]]][,!names(read_files[[names(read_files)[i]]]) %in% c(predictor)]
-      read_files.y[[i]]<-read_files[[names(read_files)[i]]][,names(read_files[[names(read_files)[i]]]) %in% c(predictor)]
-      model_metadata$xlabels[[i]]<-names(read_files[[names(read_files)[i]]])
+  future({
+    read_files<- list()
+    transformed_files<- list()
+    for (i in 1:length(file_list)){
+      # cat(file_list[[i]])
+      read_files[[names(file_list)[i]]]<- read.table(text = file_list[[i]], sep =",", header = TRUE, stringsAsFactors = FALSE)
+      colnames(read_files[[names(file_list)[i]]])[1]<-"Sample"
+      cat("COLNAMES:\n")
+      cat(colnames(read_files[[names(file_list)[i]]]))
+      cat("\n")
+      
+      
+      ###transform raw files to transformed state.
+      transformed_files[[i]]<-saved_model$pre_proc[[i]](saved_model,read_files[[i]])
     }
-  }
-  
-  cat("DFs:\n")
-  for(file in read_files){
-    print(file)
-  }
-  cat("\n Predictor_Vector:\n")
-  print(predictor_vector)
-  cat("\n File X Labels\n")
-  print(model_metadata$xlabels)
-  cat("---------\n")
-  cat("\n Read Files X Values\n")
-  print(read_files.x)
-  cat("---------\n")
-  cat("\n Read Files Y Values\n")
-  print(read_files.y)
-  cat("---------\n")
-  cat("\n")
-  read_files.x.y.concat<-utilities$drop_NAs_join(read_files.x, predictor_vector)
+    model_metadata<- list()
+    model_metadata$xlabels<-list()
+    predictor_vector<- NULL
+    read_files.x<- list()
+    read_files.y<- list()
+    if (!is.null(predictor) && length(read_files)){
+      predictor_vector<- read_files[[names(file_list)[1]]][,c("Sample",predictor)]
+    }
+    if(length(read_files)){
+      for(i in 1:length(read_files)){
+        read_files.x[[i]]<-read_files[[names(read_files)[i]]][,!names(read_files[[names(read_files)[i]]]) %in% c(predictor)]
+        read_files.y[[i]]<-read_files[[names(read_files)[i]]][,names(read_files[[names(read_files)[i]]]) %in% c(predictor)]
+        model_metadata$xlabels[[i]]<-names(read_files[[names(read_files)[i]]])
+      }
+    }
+    
+    cat("DFs:\n")
+    for(file in read_files){
+      print(file)
+    }
+    cat("\n Predictor_Vector:\n")
+    print(predictor_vector)
+    cat("\n File X Labels\n")
+    print(model_metadata$xlabels)
+    cat("---------\n")
+    cat("\n Read Files X Values\n")
+    print(read_files.x)
+    cat("---------\n")
+    cat("\n Read Files Y Values\n")
+    print(read_files.y)
+    cat("---------\n")
+    cat("\n")
+    as.data.frame(saved_model$main_proc(transformed_files))
+    
+  })
+  # read_files.x.y.concat<-utilities$drop_NAs_join(read_files.x, predictor_vector)
 }
 
 #* @get /predict/dimensions
